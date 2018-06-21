67a6b65084f578b22e9be9bc1a79f8ff4d8123d7
mikeroswell <mike.roswell@gmail.com>
2018-06-21 16:17:01 -0400

    simulation that says measure 50... I mean 10 IT

---
+++ scripts/CV_simulation.R
@@ -0,0 +1,19 @@
+# look at observed CV with sample size given sigma, mu
+library(plyr)
+library(tidyverse)
+
+sigma<-seq(.1,.3,.05)
+mu<-seq(1,4,1)
+
+out<-ldply(lapply(rep(seq(1,200,1),100), function(n){ldply(lapply(sigma, function(x){ldply(lapply(mu, function(y){
+  raw<-rnorm(n,y,y*x)
+  av<-mean(raw)
+  std<-sd(raw)
+  CV<-std/av
+  return(cbind("n"=n, "mu"=y, "sigma"=x*y,"CV_obs"=CV))
+}))}))}))
+
+
+out %>% ggplot(aes(n,CV_obs))+
+  geom_jitter(alpha=0.2)+
+  facet_wrap(~mu+sigma)+theme_classic()+ylim(c(0,1))
