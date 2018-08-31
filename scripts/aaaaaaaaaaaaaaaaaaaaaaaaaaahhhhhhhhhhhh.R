9875e8aa5122ba6f78f363214223dd888ef107b9
mikeroswell <mike.roswell@gmail.com>
2018-08-31 12:47:48 -0400

    seems like the loop works. Used ldply to combine b/c don't get map_dfr for nested lists yet

--- scripts/aaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhh.R
+++ scripts/aaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhh.R
@@ -25,21 +25,32 @@ dataflowers<-alldata %>%
 
 ###############################################################
 ## I stop working on that because the code is not working properly. It is doing what it is supposed to do, the null model, but there is a problem when it creates the list of lists. It does not give properly the length of the number of bee species present at each site and round. I don't know how to fix it
+# s<-1
+# r<-1
+# b<-1
 
-d <- lapply(1:length(levels(alldata$site)),function(x) {
-  alldatasite <- alldata%>%filter(site==levels(alldata$site)[x])
-  flowerssite <- dataflowers%>%filter(site==levels(dataflowers$site)[x])
-  beesite <- databees%>%filter(site==levels(databees$site)[x])
-    lapply(1:length(unique(alldata$sampling_round)),function(x) {
-      alldataround <- alldatasite%>%filter(sampling_round==unique(alldatasite$sampling_round)[x])
-      flowersround <- flowerssite%>%filter(sampling_round==unique(flowerssite$sampling_round)[x])
-      beesround <- beesite%>%filter(sampling_round==unique(beesite$sampling_round)[x])
-      lapply(1:length(unique(beesround$bee)),function(x){
-        alldatabee <- alldataround%>%filter(bee==unique(alldataround$bee)[x])
-        beesbee <- beesround%>%filter(bee==unique(beesround$bee)[x])
+d <- lapply(1:length(levels(alldata$site)),function(s) {
+    lapply(1:length(unique(alldata$sampling_round)),function(r) {
+      # alldatasite <- alldata%>%filter(site==levels(alldata$site)[s])
+      # flowerssite <- dataflowers%>%filter(site==levels(dataflowers$site)[s])
+      # beesite <- databees%>%filter(site==levels(databees$site)[s])
+      alldataround <- alldatasite%>%filter(sampling_round==unique(alldatasite$sampling_round)[r]&site==levels(alldata$site)[s] )
+      flowersround <- flowerssite%>%filter(sampling_round==unique(flowerssite$sampling_round)[r]&site==levels(dataflowers$site)[s])
+      beesround <- beesite%>%filter(sampling_round==unique(beesite$sampling_round)[r]&site==levels(databees$site)[s])
+      lapply(1:length(unique(beesround$bee)),function(b){
+        alldatabee <- alldataround%>%filter(bee==unique(alldataround$bee)[b]) %>% droplevels()
+        beesbee <- beesround%>%filter(bee==unique(beesround$bee)[b])
         sampledflowers <- sample(flowersround$depth, length(alldatabee$bee), replace = T, prob = flowersround$abundance)
         sampleddifferences <- sampledflowers - beesbee$tongue
-        return(data.frame(sampleddifferences,tongue=rep(beesbee$tongue,length(sampleddifferences))))
+        # print(sampleddifferences)
+        # print(rep(beesbee$tongue,length(sampleddifferences)))
+        return(
+          data.frame(sampleddifferences,
+                     tongue=rep(beesbee$tongue,length(sampleddifferences)),
+                     bee=rep(beesbee$bee, length(sampleddifferences)),
+                     site=rep(unique(alldata$site)[s], length(sampleddifferences)),
+                     sampleround=rep(unique(alldata$sampling_round)[r], length(sampleddifferences)))
+           )
         # length(alldatabee$bee)
         # length(sampledflowers)
       })
@@ -48,44 +59,23 @@ d <- lapply(1:length(levels(alldata$site)),function(x) {
   })
 
 
-x <- c(32,35,28,25,36)
+library(plyr)
 
-nrow(beefiltered)
-beefiltered <- databees%>%filter(site=="Baldpate" & sampling_round=="2")%>%
-  group_by(bee)%>%
-  summarize(tongue=mean(tongue),abund=n())
-
+bigout<-ldply(lapply(d, function(x){
+  ldply(lapply(x, function(y){
+    ldply(y)}))}))
 
 
-
-
-
-
-c[[1]][[1]][[1]]
-c[[1]][[1]][[2]]
-c[[1]][[1]][[3]]
-c[[1]][[1]][[4]]
-
-
-sort(unique(alldata$bee))
-
- rbind.fill(lapply(c, function(x)as.data.frame(t(x))))
-
-k <- map_dfr(c,rbind)
-
-z <- as.data.frame(sapply(c, unlist))
-names(z) <- unique(alldata$site)
-
-
-alldatasite <- alldata%>%filter(site=="Cold Soil" & sampling_round=="1")
-flowerssite <- dataflowers%>%filter(site=="Cold Soil"& sampling_round=="1")
-beessite <- databees%>%filter(site=="Cold Soil"& sampling_round=="1")
-# lapply(1:length(unique(alldata$sampling_round)),function(x) {
-# lapply(1:length(unique(databees$bee)),function(x){
-sampledflowers <- sample(flowerssite$depth, nrow(alldatasite), replace = T, prob = flowerssite$abundance)
-sampleddifferences <- sampledflowers - alldatasite$tongue_length.tongue
-
-hist(sampleddifferences)
-hist(c[[2]][[1]][[1]][[1]])
-length(sampleddifferences)
-length(c[[2]][[1]][[1]])
+# 
+# alldatasite <- alldata%>%filter(site=="Cold Soil" & sampling_round=="1")
+# flowerssite <- dataflowers%>%filter(site=="Cold Soil"& sampling_round=="1")
+# beessite <- databees%>%filter(site=="Cold Soil"& sampling_round=="1")
+# # lapply(1:length(unique(alldata$sampling_round)),function(x) {
+# # lapply(1:length(unique(databees$bee)),function(x){
+# sampledflowers <- sample(flowerssite$depth, nrow(alldatasite), replace = T, prob = flowerssite$abundance)
+# sampleddifferences <- sampledflowers - alldatasite$tongue_length.tongue
+# 
+# hist(sampleddifferences)
+# hist(c[[2]][[1]][[1]][[1]])
+# length(sampleddifferences)
+# length(c[[2]][[1]][[1]])
