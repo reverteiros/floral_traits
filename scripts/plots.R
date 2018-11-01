87e29aa98fe726ad3f107c765db22e2c4279ef32
mike.roswell <mikeroswell@nbp-7-72.nbp.ruw.rutgers.edu>
2018-11-01 10:48:53 -0400

    Went through this script. Now plots all data (MS fig 1) and histograms of depth and tongue length by species rather than individual, and also has a histrogram of all differences that I think we won't use.

--- scripts/plots.R
+++ scripts/plots.R
@@ -5,11 +5,16 @@ library(ggplot2)
 library(ggExtra)
 
 
+###################
+# MS fig 1
+### show all data
+
 #### Plot tongue vs depth with histograms at margins
-g <- ggplot(generaldata, aes(y=depth, x=tongue_length.tongue)) + 
-  geom_jitter(alpha=0.1, height=0.1) + 
+g <- ggplot(generaldata, aes(depth, tongue_length.tongue)) + 
+  geom_jitter(alpha=0.05, height=0.2, width=0.1, size=0.6) + 
   theme_classic() +
-  labs(y="Flower depth (mm)", x="Bee tongue length (mm)")
+  coord_fixed()+
+  labs(x="Flower depth (mm)", y="Bee tongue length (mm)")
 
 ggMarginal(g, type = "histogram", fill="transparent")
 
@@ -29,65 +34,42 @@ ggMarginal(g, type = "histogram", fill="transparent")
 # barplot(sort(histbeespecies$abundance,decreasing=T))
 
 #all bees tongue length histogram
-hist(generaldata$tongue_length.tongue, xlab="Tongue length (mm)",main="",breaks=seq(0, 14, by = 1))
+# hist(generaldata$tongue_length.tongue, xlab="Tongue length (mm)",main="",breaks=seq(0, 14, by = 1))
 # #all bees improved IT histogram
 # hist(generaldata$IT_improved, xlab="IT (mm)",main="")
 #bee species tongue length histogram
-hist(histbeespecies$tongue, xlab="Tongue length (mm)",main="",breaks=seq(0, 14, by = 1))
+#This object no longer exists
+# hist(histbeespecies$tongue, xlab="Tongue length (mm)",main="",breaks=seq(0, 14, by = 1))
+
+### if we want, distribution of tongue lenghts by species, rather than by individual
+generaldata %>% group_by(bee) %>% summarize(tongue=mean(tongue_length.tongue)) %>% ggplot(aes(tongue))+
+  geom_histogram(bins=12)+
+  theme_classic()+
+  labs(x="tongue length (mm)", y="# species")
+
+
 # #bee species IT histogram
 # hist(histbeespecies$IT, xlab="IT (mm)",main="")
 #all visits corolla depth histogram
-hist(generaldata$depth, xlab="Flower depth (mm)",main="",breaks=seq(0, 31, by = 1))
+# hist(generaldata$depth, xlab="Flower depth (mm)",main="",breaks=seq(0, 31, by = 1))
 # #all visits corolla width histogram
 # hist(generaldata$width, xlab="Flower width (mm)",main="")
 #flower species corolla depth histogram
-hist(histplantspecies$depth, xlab="Flower depth (mm)",main="",breaks=seq(0, 31, by = 1))
+### if we want, distribution of tongue lenghts by species, rather than by individual
+generaldata %>% group_by(plant_gs) %>% summarize(depth=mean(depth)) %>% ggplot(aes(depth))+
+  geom_histogram(bins=12)+
+  theme_classic()+
+  labs(x="corolla depth (mm)", y="# species")
+
+
 # #flower species corolla width histogram
-# hist(histplantspecies$width, xlab="Flower width (mm)",main="")
+
 #all bees difference between depth and tongue
-hist(generaldata$difference, xlab="Tongue length - flower depth (mm)",main="",breaks=seq(-13, 32, by = 1))
+hist(generaldata$difference, xlab="tongue length - flower depth (mm)",main="")
 #this looks impressively balanced around 0! What is null i.e. totally random visits? 
 
 
 
-# 
-# #### Plot quantile regresssion
-# ggplot(generaldata, aes(y=tongue_length.tongue, x=depth)) + 
-#   geom_jitter(alpha=0.1, height=0.1) + 
-#   theme_classic() 
-# 
-# #### Density plots of flowers with quantiles
-# probs <- c(0.1, 0.25, 0.5, 0.75, 0.9)
-# dens <- density(generaldata$depth)
-# df <- data.frame(x=dens$x, y=dens$y)
-# quantiles <- quantile(generaldata$depth, prob=probs)
-# df$quant <- factor(findInterval(df$x,quantiles))
-# ggplot(df, aes(x,y)) + geom_line() + geom_ribbon(aes(ymin=0, ymax=y, fill=quant)) + scale_x_continuous(breaks=quantiles) + scale_fill_brewer(guide="none")
-# 
-# #### Density plots of bees with quantiles
-# generaldata2 <- generaldata[!is.na(generaldata$tongue_length.tongue),]
-# 
-# probs <- c(0.1, 0.25, 0.5, 0.75, 0.9)
-# dens <- density(generaldata2$tongue_length.tongue)
-# df <- data.frame(x=dens$x, y=dens$y)
-# quantiles <- quantile(generaldata2$tongue_length.tongue, prob=probs)
-# df$quant <- factor(findInterval(df$x,quantiles)) 
-# ggplot(df, aes(x,y)) + geom_line() + geom_ribbon(aes(ymin=0, ymax=y, fill=quant)) + scale_x_continuous(breaks=quantiles) + scale_fill_brewer(guide="none")
-# 
-# 
-# ## separating longue-tongued bees and short-tongued bees
-# 
-# longuetongues <- dplyr::filter(generaldata, tongue_length.tongue > 8)
-# shorttongues <- dplyr::filter(generaldata, tongue_length.tongue < 4)
-# 
-# histshortbeespecies<-shorttongues %>%
-#   group_by(bee) %>%
-#   summarize(tongue=mean(tongue_length.tongue), integertongue=mean(as.integer(tongue_length.tongue)),IT=mean(IT_improved),abundance=n())
-# 
-# histlongbeespecies<-longuetongues %>%
-#   group_by(bee) %>%
-#   summarize(tongue=mean(tongue_length.tongue), integertongue=mean(as.integer(tongue_length.tongue)),IT=mean(IT_improved),abundance=n())
-# 
 # 
 # ### plot means and sd of depths visited by each bee species, weighted by abundance
 # dat %>% group_by(bee, sr, tongue_length.tongue) %>% summarize(mfd=mean(depth), ABUND=n(), vari=sd(depth)) %>% ggplot(aes(tongue_length.tongue,mfd))+
