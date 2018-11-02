2fef662cc4839aae39f4fdb0a1baecac82fc4ac8
mike.roswell <mikeroswell@network.fios-router.home>
2018-11-02 09:53:49 -0400

    super confused why my graph looks different from Sara's

--- scripts/null model trait matching.R
+++ scripts/null model trait matching.R
@@ -11,7 +11,15 @@ dat<-generaldata %>% mutate(sr=paste(sampling_round, site))
 out<-vector("list", length(unique(dat$sr)))
             
 # read and manipulate data
-out<-lapply(1:length(unique(dat$sr)), function(y){
+
+### for Parallel computing on Mac/Linux, comment out for windows or fix
+library(parallel)
+no_cores <- detectCores() - 1
+cl <- makeCluster(no_cores, type="FORK", outfile="", XDR=F)
+clusterExport(cl=cl, varlist=ls(), envir=environment())
+out<-parLapply(cl, 1:length(unique(dat$sr)), function(y){
+#this needs to be turned back on for windows
+# out<-lapply(1:length(unique(dat$sr)), function(y){
   sub <- dat %>% filter(sr==unique(dat$sr)[y])
 
 ### create objects at the species level, with abundance, and mean traits 
@@ -51,6 +59,7 @@ datamatrix$newdifference <- filtered$newdifference
 
 return(dplyr::mutate(datamatrix, sr=unique(dat$sr)[y]))
 })
+stopCluster(cl)
 
 
 
