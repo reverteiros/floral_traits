cdb5d4740a70fcca3cb0ac514cb5d8d2e1a8e268
mike.roswell <mikeroswell@network.fios-router.home>
2018-11-04 20:01:23 -0500

    Kinda rewrote this hope it still does everything we wanted

--- scripts/trait matching test filtered site round but global result.R
+++ scripts/trait matching test filtered site round but global result.R
@@ -7,162 +7,96 @@ source("scripts/traits.R")
 library(ggplot2)
 library(purrr)
 
-
-## Choose at the beginning if we want to remove small bees that can crawl in
-generaldata <- generaldata %>% 
-  dplyr::filter(., beewider== "true")
-
-
+#run null model
 source("scripts/null model trait matching.R")
 
 # Filter species that appear less than 5 times
-datatotalbees<-datatotal %>%
+obs<-dat %>%
+  mutate(big=(IT_improved-width)>0, zeroed=ifelse(difference<0, difference*big, difference),deleted=ifelse(difference<0, ifelse(big, difference, NA), difference), iter=rep(7777777, length(dat$bee)))  %>% 
   group_by(bee) %>%
-  summarize(abundance=n()) %>%
-  dplyr::filter(., abundance > 4)
+  # summarize(raw_mismatch=mean(difference<0), zeroed=mean(zeroed<0), deleted=mean(deleted<0, na.rm=T), abundance=n()) %>%
+  filter(length(uniqueID) > 4) %>% select(bee, iter, difference, big, zeroed, deleted)
 
-datatotal <- dplyr::inner_join(datatotal, datatotalbees, by = "bee")
+upr<-function(x){quantile(x, .975, na.rm=T)}
+lwr<-function(x){quantile(x, .025, na.rm=T)}
+av<-function(x){mean(x, na.rm=T)}
+std<-function(x){sd(x, na.rm=T)}
+z<-function(obs, xpctd, xvar){(obs-xpctd)/xvar}
 
+
+#combine null and observed
+# combo <- dplyr::inner_join(datatotal, obs, by = "bee")
+comb<-bind_rows(obs, datatotal %>% transmute(bee, iter, difference=raw_t_minuts_d, deleted=remove, zeroed=zero, tongue, iter, sr))
+
+
+nmod <- comb %>% select(-sr) %>% 
+  group_by(bee, tongue, iter) %>%
+  summarize_all(funs(av, std))
 ## Now we have the null model distributions per each interaction with the flowers that each individual bee can face at the site-round that was present, it's time to work with the data. 
 
-############### Test for trait matching with the entire network 
-####(approach similar to Sazatornil et al 2016)
-means <- numeric(iterations+2)
-sds <- numeric(iterations+2)
-means[iterations+1] <- mean((datatotal[,(iterations+3)]))#add observed value
-sds[iterations+1] <- sd(datatotal[,(iterations+3)])#add observed value
-means[iterations+2] <- mean((datatotal[,(iterations+4)]))#add observed value assuming 0 difference for small bees
-sds[iterations+2] <- sd(datatotal[,(iterations+4)])#add observed value assuming 0 difference for small bees
-
-# generate means and sd of each entire null network, 999 replicates
-for(i in 1:iterations){
-  means[i] <- mean(datatotal[,i])
-  sds[i] <- sd(datatotal[,i])
-}
-
-#Graphs to test if observed values are different from random
-hist(means[1:iterations],main="Mean difference (tongue length minus flower depth, mm)",xlab="",ylab="")
-abline(v=means[iterations+1])
-# run both lines together or doesn't work
-hist(sds[1:iterations],xlim=c(4,7),main=" SD difference (tongue length minus flower depth, mm)",xlab="",ylab="")
-abline(v=sds[iterations+1])
-## mean is in the random distribution, but observed sd is very different than random sds.
-## observed sd is way smaller than random, indicating some trait matching.
-
-
-#Graphs to test if observed values are different from random assuming 0 difference for small bees 
-hist(means[1:iterations],xlim=c(-1,1),main="Mean difference (tongue length minus flower depth, mm)",xlab="",ylab="")
-abline(v=means[iterations+2])
-# run both lines together or doesn't work
-hist(sds[1:iterations],xlim=c(4,7),main=" SD difference (tongue length minus flower depth, mm)",xlab="",ylab="")
-abline(v=sds[iterations+2])
 
 
 
 ######### Test for trait matching at the species level. 
 ## Do it for each bee species, to see if some tongues trait match more
 
-nullperbee<-datatotal[order(datatotal$bee),] 
-nullperbee$bee <- factor(nullperbee$bee)
-nullperbee$beenumeric <- as.numeric(nullperbee$bee)
-
-observed<-nullperbee %>%
-  group_by(bee) %>%
-  summarize(mean_obs=mean(difference),sd_obs=sd(difference),mean_newdif_obs=mean(newdifference),sd_newdif_obs=sd(newdifference),abundance=n())
-
-observed <- dplyr::left_join(observed, databees,by=c("bee","abundance"))
-
-
+bysp<-nmod %>% filter(iter!=7777777) %>% 
+  group_by(bee, tongue) %>% summarize_all(funs(av, std, upr, lwr))%>% 
+  right_join(nmod %>% filter(iter==7777777), by="bee")
 ########### MEANS
-meanpersp <- matrix(ncol = 999,nrow = nrow(observed))
-meanpersp <- as.data.frame(meanpersp)
 
-for(i in 1:nrow(observed)){
-  filtre <- dplyr::filter(nullperbee, beenumeric == i)
-  filtre2 <- filtre[,1:999]
-  meanpersp[i,] <- apply(filtre2,2,mean)
-}
 
-meanpersp$bee <- unique(observed$bee)
-
-# Quantiles of the null models
-meanpersp$quantile975 <- apply(meanpersp[,1:999],1,quantile,probs=c(.975))
-meanpersp$quantile25 <- apply(meanpersp[,1:999],1,quantile,probs=c(.025))
-meanpersp$mean <- apply(meanpersp[,1:999],1,mean)
-meanpersp$sd <- apply(meanpersp[,1:999],1,sd)
-
-# when we did summary we had a column of tongues, but it turned to ones. Remove and insert again
-datameans <- dplyr::left_join(meanpersp,observed,"bee")
-
-# when we did summary we had a column of tongues, but it turned to ones. Remove and insert again
-datameans$tongue <- NULL
-
-# Generate a matrix with each bee one time, with tongue length of each
-databeesall<-generaldata %>%
-  group_by(bee) %>%
-  summarize(tongue=mean(tongue_length.tongue),abundance=n()) 
-
-# Include tongue length to dataframe
-datamatrixmeans <- dplyr::left_join(datameans,databeesall,"bee")
-
-# Plot graph with means of proportion of flowers longer than tongues derived from null model with error bars and real data
-ggplot(datamatrixmeans, aes(y=mean_obs, x=tongue)) + 
-  geom_point(size=1.5,color="red") +
-  geom_errorbar(aes(ymin=quantile25, ymax=quantile975), position = position_dodge(0.3)) +
-  theme_bw(base_size=16) + 
-  labs(y=" Mean difference (tongue length minus flower depth, mm)",x="Bee tongue length (mm)") +
+rawmean<-bysp %>% ggplot(aes(tongue.x)) + 
+  geom_point(aes(y=difference_av_av)) +
+  geom_errorbar(aes(ymin=difference_av_lwr, ymax=difference_av_upr)) +
+  geom_point(aes(y=difference_av),size=1.5,color="red") +
+  scale_x_log10()+
+  labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
 
-# Plot graph with means of proportion of flowers longer than tongues derived from null model with error bars and real data
-ggplot(datamatrixmeans, aes(y=mean_newdif_obs, x=tongue)) + 
-  geom_point(size=1.5,color="red") +
-  geom_errorbar(aes(ymin=quantile25, ymax=quantile975), position = position_dodge(0.3)) +
-  theme_bw(base_size=16) + 
-  labs(y=" Mean difference (tongue length minus flower depth, mm)",x="Bee tongue length (mm)") +
+rawmeanz<-bysp %>% ggplot(aes(tongue.x, z(difference_av,difference_av_av, difference_av_std))) + 
+  geom_point() +
+  scale_x_log10()+
+  labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
+  geom_hline(yintercept=0)+
+  geom_hline(yintercept=-1.96)+
+  geom_hline(yintercept=1.96)+
+  theme_classic()
+
+zeromean<-bysp %>% ggplot(aes(tongue.x)) + 
+  geom_point(aes(y=zeroed_av_av)) +
+  geom_errorbar(aes(ymin=zeroed_av_lwr, ymax=zeroed_av_upr)) +
+  geom_point(aes(y=zeroed_av),size=1.5,color="red") +
+  scale_x_log10()+
+  labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
+  theme_classic()
+
+zeromeanz<-bysp %>% ggplot(aes(tongue.x, z(zeroed_av,zeroed_av_av, zeroed_av_std))) + 
+  geom_point() +
+  scale_x_log10()+
+  labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
+  geom_hline(yintercept=0)+
+  geom_hline(yintercept=-1.96)+
+  geom_hline(yintercept=1.96)+
   theme_classic()
 
 
-########### SD
-sdpersp <- matrix(ncol = 999,nrow = nrow(observed))
-sdpersp <- as.data.frame(sdpersp)
-
-for(i in 1:nrow(observed)){
-  filtre <- dplyr::filter(nullperbee, beenumeric == i)
-  filtre2 <- filtre[,1:999]
-  sdpersp[i,] <- apply(filtre2,2,sd)
-}
-
-sdpersp$bee <- unique(nullperbee$bee)
-
-# Mean,  and quantiles of the null models
-sdpersp$quantile975 <- apply(sdpersp[,1:999],1,quantile,probs=c(.975))
-sdpersp$quantile25 <- apply(sdpersp[,1:999],1,quantile,probs=c(.025))
-sdpersp$sd <- apply(sdpersp[,1:999],1,sd)
-sdpersp$mean <- apply(sdpersp[,1:999],1,mean)
-
-# when we did summary we had a column of tongues, but it turned to ones. Remove and insert again
-datasd <- dplyr::left_join(sdpersp,observed,"bee")
-
-# when we did summary we had a column of tongues, but it turned to ones. Remove and insert again
-datasd$tongue <- NULL
-
-# Include tongue length to dataframe
-datamatrixsd <- dplyr::left_join(datasd,databeesall,"bee")
-
-# Plot graph with sd of proportion of flowers longer than tongues derived from null model with error bars and real data
-ggplot(datamatrixsd, aes(y=sd_obs, x=tongue)) + 
-  geom_point(size=1.5,color="red") +
-  geom_errorbar(aes(ymin=quantile25, ymax=quantile975), position = position_dodge(0.3)) +
-  theme_bw(base_size=16) + 
-  labs(y=" SD difference (tongue length minus flower depth, mm)",x="Bee tongue length (mm)") +
+rawsd<-bysp %>% ggplot(aes(tongue.x)) + 
+  geom_point(aes(y=difference_std_av)) +
+  geom_errorbar(aes(ymin=difference_std_lwr, ymax=difference_std_upr)) +
+  geom_point(aes(y=difference_std),size=1.5,color="red") +
+  scale_x_log10()+
+  labs(y="sd(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
 
-ggplot(datamatrixsd, aes(y=sd_newdif_obs, x=tongue)) + 
-  geom_point(size=1.5,color="red") +
-  geom_errorbar(aes(ymin=quantile25, ymax=quantile975), position = position_dodge(0.3)) +
-  theme_bw(base_size=16) + 
-  labs(y=" SD difference (tongue length minus flower depth, mm)",x="Bee tongue length (mm)") +
+zerosd<-bysp %>% ggplot(aes(tongue.x)) + 
+  geom_point(aes(y=zeroed_std_av)) +
+  geom_errorbar(aes(ymin=zeroed_std_lwr, ymax=zeroed_std_upr)) +
+  geom_point(aes(y=zeroed_std),size=1.5,color="red") +
+  scale_x_log10()+
+  labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
+
 
 
 
@@ -246,4 +180,31 @@ observed %>% mutate(absdif=abs(mean_newdif_obs)) %>% ggplot(aes(x=abs(mean_newdi
 
 a <- lm(abs(observed$mean_newdif_obs)~(observed$sd_newdif_obs))
 summary(a)
+############### Test for trait matching with the entire network 
+####(approach similar to Sazatornil et al 2016)
+means <- numeric(iterations+2)
+sds <- numeric(iterations+2)
+means[iterations+1] <- mean((datatotal[,(iterations+3)]))#add observed value
+sds[iterations+1] <- sd(datatotal[,(iterations+3)])#add observed value
+means[iterations+2] <- mean((datatotal[,(iterations+4)]))#add observed value assuming 0 difference for small bees
+sds[iterations+2] <- sd(datatotal[,(iterations+4)])#add observed value assuming 0 difference for small bees
+
+
+#Graphs to test if observed values are different from random
+hist(means[1:iterations],main="Mean difference (tongue length minus flower depth, mm)",xlab="",ylab="")
+abline(v=means[iterations+1])
+# run both lines together or doesn't work
+hist(sds[1:iterations],xlim=c(4,7),main=" SD difference (tongue length minus flower depth, mm)",xlab="",ylab="")
+abline(v=sds[iterations+1])
+## mean is in the random distribution, but observed sd is very different than random sds.
+## observed sd is way smaller than random, indicating some trait matching.
+
+
+#Graphs to test if observed values are different from random assuming 0 difference for small bees 
+hist(means[1:iterations],xlim=c(-1,1),main="Mean difference (tongue length minus flower depth, mm)",xlab="",ylab="")
+abline(v=means[iterations+2])
+# run both lines together or doesn't work
+hist(sds[1:iterations],xlim=c(4,7),main=" SD difference (tongue length minus flower depth, mm)",xlab="",ylab="")
+abline(v=sds[iterations+2])
+
 
