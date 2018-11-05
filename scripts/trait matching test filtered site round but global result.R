b1f9f0c627d7ba90a170c733bbca585bccaf5f6a
mike.roswell <mikeroswell@network.fios-router.home>
2018-11-05 03:07:05 -0500

    all as sent to lab group

--- scripts/trait matching test filtered site round but global result.R
+++ scripts/trait matching test filtered site round but global result.R
@@ -4,8 +4,7 @@
 
 source("scripts/traits.R")
 
-library(ggplot2)
-library(purrr)
+library(cowplot)
 
 #run null model
 source("scripts/null model trait matching.R")
@@ -49,14 +48,16 @@ bysp<-nmod %>% filter(iter!=7777777) %>%
 rawmean<-bysp %>% ggplot(aes(tongue.x)) + 
   geom_point(aes(y=difference_av_av)) +
   geom_errorbar(aes(ymin=difference_av_lwr, ymax=difference_av_upr)) +
-  geom_point(aes(y=difference_av),size=1.5,color="red") +
+  geom_point(aes(y=difference_av),color="red") +
   scale_x_log10()+
+  scale_y_continuous(limits=c(-14,10))+
   labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
 
 rawmeanz<-bysp %>% ggplot(aes(tongue.x, z(difference_av,difference_av_av, difference_av_std))) + 
   geom_point() +
   scale_x_log10()+
+  scale_y_continuous(limits=c(-22,22))+
   labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
   geom_hline(yintercept=0)+
   geom_hline(yintercept=-1.96)+
@@ -66,32 +67,44 @@ rawmeanz<-bysp %>% ggplot(aes(tongue.x, z(difference_av,difference_av_av, differ
 zeromean<-bysp %>% ggplot(aes(tongue.x)) + 
   geom_point(aes(y=zeroed_av_av)) +
   geom_errorbar(aes(ymin=zeroed_av_lwr, ymax=zeroed_av_upr)) +
-  geom_point(aes(y=zeroed_av),size=1.5,color="red") +
+  geom_point(aes(y=zeroed_av),color="red") +
   scale_x_log10()+
+  scale_y_continuous(limits=c(-14,10))+
   labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
 
 zeromeanz<-bysp %>% ggplot(aes(tongue.x, z(zeroed_av,zeroed_av_av, zeroed_av_std))) + 
   geom_point() +
   scale_x_log10()+
+  scale_y_continuous(limits=c(-22,22))+
   labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
   geom_hline(yintercept=0)+
   geom_hline(yintercept=-1.96)+
   geom_hline(yintercept=1.96)+
   theme_classic()
 
+#####
+#fig 3
+#####
+pdf(file="figures/fig3.pdf", 9,8)
+plot_grid(rawmean, rawmeanz, zeromean, zeromeanz, ncol=2, labels="auto")
+dev.off()
+
+
 rawsd<-bysp %>% ggplot(aes(tongue.x)) + 
   geom_point(aes(y=difference_std_av)) +
   geom_errorbar(aes(ymin=difference_std_lwr, ymax=difference_std_upr)) +
-  geom_point(aes(y=difference_std),size=1.5,color="red") +
+  geom_point(aes(y=difference_std),color="red") +
   scale_x_log10()+
+  scale_y_continuous(limits=c(0,14))+
   labs(y="sd(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
 
 rawsdz<-bysp %>% ggplot(aes(tongue.x, z(difference_std,difference_std_av, difference_std_std))) + 
   geom_point() +
   scale_x_log10()+
-  labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
+  scale_y_continuous(limits=c(-15,15))+
+  labs(y="z-score: sd(tongue-corolla), mm",x="bee tongue length (mm)") +
   geom_hline(yintercept=0)+
   geom_hline(yintercept=-1.96)+
   geom_hline(yintercept=1.96)+
@@ -101,7 +114,8 @@ rawsdz<-bysp %>% ggplot(aes(tongue.x, z(difference_std,difference_std_av, differ
 zerosd<-bysp %>% ggplot(aes(tongue.x)) + 
   geom_point(aes(y=zeroed_std_av)) +
   geom_errorbar(aes(ymin=zeroed_std_lwr, ymax=zeroed_std_upr)) +
-  geom_point(aes(y=zeroed_std),size=1.5,color="red") +
+  geom_point(aes(y=zeroed_std),color="red") +
+  scale_y_continuous(limits=c(0,14))+
   scale_x_log10()+
   labs(y="mean(tongue-corolla), mm",x="bee tongue length (mm)") +
   theme_classic()
@@ -109,13 +123,16 @@ zerosd<-bysp %>% ggplot(aes(tongue.x)) +
 zerosdz<-bysp %>% ggplot(aes(tongue.x, z(zeroed_std,zeroed_std_av, zeroed_std_std))) + 
   geom_point() +
   scale_x_log10()+
-  labs(y="z-score(tongue-corolla), mm",x="bee tongue length (mm)") +
+  scale_y_continuous(limits=c(-15,15))+
+  labs(y="z-score: sd(tongue-corolla), mm",x="bee tongue length (mm)") +
   geom_hline(yintercept=0)+
   geom_hline(yintercept=-1.96)+
   geom_hline(yintercept=1.96)+
   theme_classic()
 
-
+pdf(file="figures/fig4.pdf", 9,8)
+plot_grid(rawsd, rawsdz, zerosd, zerosdz, ncol=2, labels="auto")
+dev.off()
 #look at, e.g. correlation between effect size for SD or mean (tongue-corolla) from tongue
 # a <- lm(z(difference_std,difference_std_av, difference_std_std)~log(tongue.x), data=bysp)
 # plot(a)
@@ -129,15 +146,35 @@ zerosdz<-bysp %>% ggplot(aes(tongue.x, z(zeroed_std,zeroed_std_av, zeroed_std_st
 k<-obs %>% group_by(bee) %>% summarize(ave=abs(av(difference)), varx=std(difference))
   
 mean_variance_scaling<-bysp %>% ggplot(aes(abs(difference_av_av), difference_std_av))+
-  geom_point()+
+  # geom_point()+
   # geom_errorbar(aes(x=abs(difference_av_av),ymin=difference_std_lwr, ymax=difference_std_upr))+
   # geom_errorbarh(aes(y=difference_std_av,xmin=abs(difference_av_lwr), xmax=abs(difference_av_upr)))+
   geom_point(aes(ave, varx), data=k,color="red")+ 
-  # scale_x_continuous(limits=c(0,8))+
+  scale_x_continuous(limits=c(0,12))+
+  scale_y_continuous(limits=c(0,8))+
+  # geom_jitter(height=0.1) + 
+  theme_classic()+
+  # geom_smooth(aes(ave, varx), data=k,method=lm)+
+  labs(x="Mean (tongue-corolla), mm", y="sd(tongue-corolla)")
+
+mean_variance_null<-bysp %>% ggplot(aes(abs(difference_av_av), difference_std_av))+
+  geom_point()+
+  # geom_errorbar(aes(x=abs(difference_av_av),ymin=difference_std_lwr, ymax=difference_std_upr))+
+  # geom_errorbarh(aes(y=difference_std_av,xmin=abs(difference_av_lwr), xmax=abs(difference_av_upr)))+
+  # geom_point(aes(ave, varx), data=k)+#,color="red")+ 
+  scale_x_continuous(limits=c(0,12))+
+  scale_y_continuous(limits=c(0,8))+
   # geom_jitter(height=0.1) + 
   theme_classic()+
   # geom_smooth(method=lm)+
-  labs(x="Mean (tongue-corolla), mm", y="sd(tongue-corolla), mm")
+  labs(x="Mean (tongue-corolla), mm", y="sd(tongue-corolla)")
+
+################
+### fig 5
+#######
+pdf(file="figures/fig5.pdf",9,4)
+plot_grid(mean_variance_scaling, mean_variance_null, labels="auto")
+dev.off()
 
 c <- lm(varx~ave, data=(obs %>% group_by(bee) %>% summarize(ave=abs(av(difference)), varx=std(difference))))
 plot(c)
@@ -161,6 +198,11 @@ global_hist<-comb %>% mutate(datasrc=iter<7777777) %>% ggplot(aes(difference,fil
   scale_fill_discrete(labels=c("observed", "null model"), name="")+
   # geom_freqpoly(stat="density")+
   geom_density(alpha=0.5, kernel="epanechnikov", n=60)+
-  labs(x="tongue-corolla", y="visit \"density\" (height proportional to visit frequency)")+
+  labs(x="tongue length minus flower depth, mm", y="visit \"density\" (height proportional to visit frequency)")+
   # theme(legend.title = element_blank())+
   theme_classic()
+
+pdf(file="figures/fig1.pdf")
+print(global_hist)
+dev.off()
+
